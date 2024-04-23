0c39e40750ec1637abb592377f8c5d7275fe38a6
Kavi Raj <kavir916@gmail.com>
2024-04-23 12:28:04 +0530

    Cart Update and Remove

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -1,36 +1,13 @@
-import { Fragment, useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, incrementAsync, selectItems } from './cartSlice';
-import { Dialog, Transition } from '@headlessui/react'
-import { XMarkIcon } from '@heroicons/react/24/outline'
+import {
+  deleteItemFromCartAsync,
+  selectItems,
+  updateCartAsync,
+} from './cartSlice';
+import { Dialog, Transition } from '@headlessui/react';
+import { XMarkIcon } from '@heroicons/react/24/outline';
 import { Link } from 'react-router-dom';
-
-const products = [
-  {
-    id: 1,
-    name: 'Throwback Hip Bag',
-    href: '#',
-    color: 'Salmon',
-    price: '$90.00',
-    quantity: 1,
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
-    imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
-  },
-  {
-    id: 2,
-    name: 'Medium Stuff Satchel',
-    href: '#',
-    color: 'Blue',
-    price: '$32.00',
-    quantity: 1,
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
-    imageAlt:
-      'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
-  },
-  // More products...
-]
-
-
 
 
 export default function Cart() {
@@ -38,9 +15,19 @@ export default function Cart() {
   const [open, setOpen] = useState(true)
 
   const items = useSelector(selectItems);
-  const totalAmount = items.reduce((amount, item)=>item.price*item.quantity +amount,0)
-  const totalItems = items.reduce((total, item)=>item.quantity + total,0)
+  const totalAmount = items.reduce(
+    (amount, item) => item.price * item.quantity + amount,
+    0
+  );
+  const totalItems = items.reduce((total, item) => item.quantity + total, 0);
 
+  const handleQuantity = (e, item) => {
+    dispatch(updateCartAsync({ ...item, quantity: +e.target.value }));
+  };
+
+  const handleRemove =(e, id)=>{
+    dispatch(deleteItemFromCartAsync(id))
+  }
 
   return (
     <>
@@ -51,12 +38,12 @@ export default function Cart() {
             <h1 className="text-4xl my-5 font-bold tracking-tight text-gray-900">Cart</h1>
             <div className="flow-root">
               <ul role="list" className="-my-6 divide-y divide-gray-200">
-              {items.map((product) => (
-                  <li key={product.id} className="flex py-6">
+              {items.map((item) => (
+                  <li key={item.id} className="flex py-6">
                     <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
                       <img
-                        src={product.thumbnail}
-                        alt={product.title}
+                        src={item.thumbnail}
+                        alt={item.title}
                         className="h-full w-full object-cover object-center"
                       />
                     </div>
@@ -65,25 +52,29 @@ export default function Cart() {
                       <div>
                         <div className="flex justify-between text-base font-medium text-gray-900">
                           <h3>
-                          <a href={product.href}>{product.title}</a>
+                          <a href={item.href}>{item.title}</a>
                           </h3>
-                          <p className="ml-4">${product.price}</p>
+                          <p className="ml-4">${item.price}</p>
                         </div>
-                        <p className="mt-1 text-sm text-gray-500">{product.brand}</p>
+                        <p className="mt-1 text-sm text-gray-500">{item.brand}</p>
                       </div>
                       <div className="flex flex-1 items-end justify-between text-sm">
                         <div className="text-gray-500">
                           <label htmlFor="quantity" className="inline mr-5 text-sm font-medium leading-6 text-gray-900">
                             Qty
                           </label>
-                          <select>
-                            <option value='1'>1</option>
-                            <option value='2'>2</option>
+                          <select onChange={(e) => handleQuantity(e, item)} value={item.quantity}>
+                          <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
                           </select>
                         </div>
 
                         <div className="flex">
                           <button
+                          onClick={e=>handleRemove(e,item.id)}
                             type="button"
                             className="font-medium text-indigo-600 hover:text-indigo-500"
                           >
@@ -122,13 +113,12 @@ export default function Cart() {
                 or{' '}
                 <Link to='/'>
                 <button
-                  type="button"
-                  className="font-medium text-indigo-600 hover:text-indigo-500"
-                  onClick={() => setOpen(false)}
-                >
-                  Continue Shopping
-                  <span aria-hidden="true"> &rarr;</span>
-                </button>
+                    type="button"
+                    className="font-medium text-indigo-600 hover:text-indigo-500"
+                  >
+                    Continue Shopping
+                    <span aria-hidden="true"> &rarr;</span>
+                  </button>
                 </Link>
               </p>
             </div>
